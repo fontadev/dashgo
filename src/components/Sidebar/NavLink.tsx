427d03636d589e8239e073cc06704431d934cd0e
Felipe Fontana <felipe@fontadev.com.br>
2022-12-11 12:55:15 -0300

    Ajustando navegação na aplicação.

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,20 @@
-import { Icon, Link, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import Link from "next/link";
 import { ElementType } from "react";
 
 interface NavSectionProps extends ChakraLinkProps {
     icon: ElementType;
     children: string;
+    href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavSectionProps) {
+export function NavLink({ icon, children, href, ...rest }: NavSectionProps) {
     return (
-        <Link display={'flex'} align={'center'} {...rest}>
-            <Icon as={icon} fontSize={20}></Icon>
-            <Text ml={4} fontWeight={'medium'}>{children}</Text>
+        <Link href={href} passHref>
+            <ChakraLink display={'flex'} align={'center'} {...rest}>
+                <Icon as={icon} fontSize={20}></Icon>
+                <Text ml={4} fontWeight={'medium'}>{children}</Text>
+            </ChakraLink>
         </Link>
     )
 }
--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -3,22 +3,21 @@ import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } fr
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
-export function SidebarNav()
-{
-    return(
+export function SidebarNav() {
+    return (
         <Stack
-                spacing={12}
-                align={'flex-start'}
-            >
-                <NavSection title={'GERAL'}>
-                    <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-                    <NavLink icon={RiContactsLine}>Usuários</NavLink>
-                </NavSection>
+            spacing={12}
+            align={'flex-start'}
+        >
+            <NavSection title={'GERAL'}>
+                <NavLink icon={RiDashboardLine} href='/dashboard'>Dashboard</NavLink>
+                <NavLink icon={RiContactsLine} href='/users'>Usuários</NavLink>
+            </NavSection>
 
-                <NavSection title="AUTOMAÇÃO">
-                    <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-                    <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-                </NavSection>
-            </Stack>
+            <NavSection title="AUTOMAÇÃO">
+                <NavLink icon={RiInputMethodLine} href='/forms'>Formulários</NavLink>
+                <NavLink icon={RiGitMergeLine} href='/automation'>Automação</NavLink>
+            </NavSection>
+        </Stack>
     )
 }
