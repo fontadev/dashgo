0c6675dd39f8bd0a67d31b2abbd4af59e9a00c46
Felipe Fontana <felipe@fontadev.com.br>
2022-12-11 04:17:03 -0300

    Criando a tabela.

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+export default function UserList() {
+    return (
+        <Box>
+            <Header />
+
+            <Flex w={'100%'} my={6} maxWidth={1480} mx={'auto'} px={6}>
+                <Sidebar />
+
+                <Box flex={1} borderRadius={8} bg={'gray.800'} p={8}>
+                    <Flex mb={8} justify={'space-between'} align={'center'}>
+                        <Heading size={'lg'} fontWeight={'normal'}>Usuários</Heading>
+
+                        <Button as={'a'} size={'sm'} fontSize={'sm'} colorScheme={'pink'} leftIcon={<Icon as={RiAddLine} />}>
+                            Criar novo
+                        </Button>
+                    </Flex>
+
+                    <Table colorScheme={'whiteAlpha'}>
+                        <Thead>
+                            <Tr>
+                                <Th px={'6'} color={'gray.300'} width={8}>
+                                    <Checkbox colorScheme={'pink'}></Checkbox>
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px={6}>
+                                    <Checkbox colorScheme={'pink'}></Checkbox>
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight={'bold'}>Felipe Fontana</Text>
+                                        <Text fontSize={'small'} color={'gray.300'}>felipe@fontadev.com.br</Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    11 de Dezembro, 2022
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
