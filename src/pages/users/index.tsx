6dd4f1d7e4d1583c79f153d7f48acc70fedb5561
Felipe Fontana <felipe@fontadev.com.br>
2022-12-11 12:41:19 -0300

    Trabalhando com responsividade na tabela.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header/index";
 import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar/index";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -24,17 +29,17 @@ export default function UserList() {
                     <Table colorScheme={'whiteAlpha'}>
                         <Thead>
                             <Tr>
-                                <Th px={'6'} color={'gray.300'} width={8}>
+                                <Th px={['4', '4', '6']} color={'gray.300'} width={8}>
                                     <Checkbox colorScheme={'pink'}></Checkbox>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion ?? <Th>Data de cadastro</Th>}
                                 <Th width={8}></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px={6}>
+                                <Td px={['4', '4', '6']}>
                                     <Checkbox colorScheme={'pink'}></Checkbox>
                                 </Td>
                                 <Td>
@@ -43,14 +48,14 @@ export default function UserList() {
                                         <Text fontSize={'small'} color={'gray.300'}>felipe@fontadev.com.br</Text>
                                     </Box>
                                 </Td>
-                                <Td>
-                                    11 de Dezembro, 2022
-                                </Td>
-                                <Td>
-                                <Button as={'a'} size={'sm'} fontSize={'sm'} colorScheme={'purple'} leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
-                            Editar
-                        </Button>
-                                </Td>
+                                {isWideVersion && <Td> 11 de Dezembro, 2022 </Td>}
+                                {isWideVersion &&
+                                    <Td>
+                                        <Button as={'a'} size={'sm'} fontSize={'sm'} colorScheme={'purple'} leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
+                                            Editar
+                                        </Button>
+                                    </Td>
+                                }
                             </Tr>
                         </Tbody>
                     </Table>
