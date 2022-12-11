6c614880f9df88ca7f296f1bd0227ac56d6166d4
Felipe Fontana <felipe@fontadev.com.br>
2022-12-11 13:35:17 -0300

    Configurando ActiveLink na Sidebar.

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
-import Link from "next/link";
 import { ElementType } from "react";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavSectionProps extends ChakraLinkProps {
     icon: ElementType;
@@ -10,11 +10,11 @@ interface NavSectionProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavSectionProps) {
     return (
-        <Link href={href} passHref>
+        <ActiveLink href={href} passHref>
             <ChakraLink display={'flex'} align={'center'} {...rest}>
                 <Icon as={icon} fontSize={20}></Icon>
                 <Text ml={4} fontWeight={'medium'}>{children}</Text>
             </ChakraLink>
-        </Link>
+        </ActiveLink>
     )
 }
